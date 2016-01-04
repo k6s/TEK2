c0b93b5c5f05a38b4a34219055c66885e9fa71a2
K6 <chloe.cano@epitech.eu>
2016-01-04 13:57:10 +0100

    various bugfixes, test files 0-4 ok

--- BDSH/bdsh.sh
+++ BDSH/bdsh.sh
@@ -3,9 +3,11 @@
 ## TODO: ######################################################################
 #
 #	- Escape newline character in replacement string
-#	- DB file delimiter ?
+#	- Check key for '=' and make them illegal
 #	- File order by modification time aswell as insert time ?
 #	- ./bdsh.sh  -f -k (select | del) 4  
+#	- empty key
+#	- r/w database access check
 #
 ###############################################################################
 
@@ -49,9 +51,22 @@ select_key_var()
 	key_val=$(grep "^$safe_key$delim" $db_file | cut -d $delim -f2)
 }
 
+select_val_var()
+{
+	ktmp=$key
+	key=$value
+	select_key_var
+	value=$key_val
+	key=$ktmp
+}
+
 select_key()
 {
 	n_args=$((ARGC-i))
+	if [ $n_args -ge 2 ]; then
+		echo $ESYNTAX $USAGE
+		exit 1
+	fi
 	if [ $n_args == 0 ]; then
 		safe_key='.*'
 	else
@@ -61,22 +76,34 @@ select_key()
 	if [ ${key:0} ] && [ ${key:0:1} == '$' ]; then
 		select_key_var
 		key=$key_val
-		echo newkeyval $key
-	fi
-	escape_chars_regexp
-	if [ $v_key == 1 ]; then
-		grep "^.*$safe_key.*$delim" $db_file
+		escape_chars_regexp
+
+		## key must be a complete match vs grep result for 'regular' key
+		select_regexp="^$safe_key$"
 	else
-		grep "^.*$safe_key.*$delim" $db_file | cut -d $delim -f 2
+		select_regexp="$key"
+	fi
+	select_regexp=$(grep '' $db_file | cut -d $delim -f 1 \
+		| grep "$select_regexp")
+	if [ $v_key == 1 ]; then
+		for var in $select_regexp; do
+			grep "$var$delim" $db_file
+		done
+	else
+		for var in $select_regexp; do
+			grep "$var$delim" $db_file | cut -d $delim -f 2-
+		done
 	fi
 }
 
 del_key()
 {
 	n_args=$((ARGC-i))
-	if [ $n_args == 0 ]; then
+	if [ $n_args == 0 ] || [ $n_args -ge 3 ]; then
 		echo $ESYNTAX $usage
 		exit 1
+	
+	## get key value in command line arguments
 	elif [ $n_args -ge 1 ]; then
 		key=$((i+1))
 		key=${!key}
@@ -85,14 +112,18 @@ del_key()
 			key=$key_val
 		fi
 		escape_chars_regexp
+
+		## if no value specified, delete only key's content, not whole  entry
 		if [ $n_args -eq 1 ]; then
 			sed -i "s/\(^$safe_key$delim\).*$/\1/g" $db_file
+
+		## if a value is specified, delete whole entry if provided value
+		## matches database's one, else do nothing.
 		else
 			value=$((i+2))
 			value=${!value}
 			if [ ${value:0:1} == '$' ]; then
-				select_key_var
-				value=$key_val
+				select_val_var
 			fi
 			escape_chars_repl
 			sed -i "/^$safe_value$/d" $db_file
@@ -103,8 +134,8 @@ del_key()
 put_key()
 {
 	n_args=$((ARGC-i))
-	if [[ n_args -le 1 ]]; then
-		echo "Invalid put command parameters: missing key"
+	if [[ $n_args -le 1 ]] || [ $n_args -ge 3 ]; then
+		echo $ESYNTAX $usage
 		exit 1
 	fi
 
@@ -113,7 +144,14 @@ put_key()
 	value=$((i+2))
 	key=${!key}
 	value=${!value}
-	escape_chars_regexp "$key"
+	if [ ${key:0:1} == '$' ]; then
+		select_key_var
+		key=$key_val
+	fi
+	if [ ${value:0:1} == '$' ]; then
+		select_val_var
+	fi
+	escape_chars_regexp
 
 	## if file is empty or does not exists then append new entry.
 	if [ ! -f $db_file ]; then
@@ -138,34 +176,45 @@ flush_db()
 		echo -n '' > $db_file
 	else
 		echo $EFNOENT $db_file
+		exit 1
 	fi
 }
 
 get_cmdline()
 {
-	i=0
-	while [ $i -le $ARGC ]; do
-		case ${!i} in
-			"put")
-				put_key $ARGV
-				;;
-			"flush")
-				flush_db
-				;;
-			"select")
-				select_key $ARGV
-				;;
-			"del")
-				del_key $ARGV
-		esac
-		i=$((i+1))
-	done
+	i=$((OPTIND))
+	case ${!i} in
+		"put")
+			put_key $ARGV
+			;;
+		"flush")
+			flush_db
+			;;
+		"select")
+			if [ ! -f $db_file ]; then
+				echo $EFNOENT $db_file
+				exit 1
+			fi
+			select_key $ARGV
+			;;
+		"del")
+			if [ ! -f $db_file ]; then
+				echo $EFNOENT $db_file
+				exit 1
+			fi
+			del_key $ARGV
+			;;
+		*)
+			echo $ESYNTAX $usage
+			exit 1
+	esac
 }
 
 get_options()
 {
-	local OPTIND opt a
-	while getopts 'f:k' opt; do
+	OPTIND=1
+	local OPTARG
+	while getopts 'f:k' opt 2> /dev/null ; do
 		case $opt in
 			f)
 				db_file=$OPTARG
@@ -174,11 +223,11 @@ get_options()
 				v_key=1
 				;;
 			\?)
-				echo "Invalid option: -$OPTARG"
+				echo $ESYNTAX "Invalid option in command arguments"
 				exit 1
 				;;
 			:)
-				echo "Option -$OPTARG requires an argument"
+				echo $ESYNTAX "Option requires an argument"
 				exit 1
 				;;
 		esac
