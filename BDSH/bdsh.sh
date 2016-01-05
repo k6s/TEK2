454c41565e7d7576a634a042e03e80c1ba26f83b
K6 <chloe.cano@epitech.eu>
2016-01-05 04:28:36 +0100

    tests 1-4 ok except for delimiter char present in key

--- BDSH/bdsh.sh
+++ BDSH/bdsh.sh
@@ -16,6 +16,7 @@ delim='='
 v_key=0
 
 EFNOENT='No base found : file '
+EKNOENT='No such key : '
 ESYNTAX='Syntax error : '
 
 usage='Usage : bdsh.sh [-k] [-f db_file]
@@ -28,10 +29,10 @@ flush)'
 ## Concerned chars are delimiter (/) and \[]^$.*
 escape_chars_regexp()
 {
-	safe_key=$(echo $key | sed \
+	safe_key=$(echo "${key}" | sed \
 		-e 's/\\/\\\\/g;s/\//\\\//g' \
 		-e 's/\[/\\\[/g;s/\]/\\\]/g' \
-	   	-e 's/\^/\\\^/g;s/\$/\\$/g' \
+	   	-e 's/\^/\\\^/g;s/\$/\\\$/g' \
 		-e 's/\./\\\./g;s/\*/\\\*/g')
 }
 
@@ -48,7 +49,12 @@ select_key_var()
 {
 	key=${key:1}
 	escape_chars_regexp
-	key_val=$(grep "^$safe_key$delim" $db_file | cut -d $delim -f2)
+	key_val=$(grep "^$safe_key$delim" "$db_file")
+	if [ "k$key_val" == "k" ]; then
+		echo $EKNOENT $key
+		exit 1
+	fi
+	key_val=$(grep "^$safe_key$delim" "$db_file" | cut -d $delim -f2)
 }
 
 select_val_var()
@@ -73,7 +79,7 @@ select_key()
 		key=$((i+1))
 		key=${!key}
 	fi
-	if [ ${key:0} ] && [ ${key:0:1} == '$' ]; then
+	if [ "k$key" != "k" ] && [ ${key:0:1} == '$' ]; then
 		select_key_var
 		key=$key_val
 		escape_chars_regexp
@@ -83,15 +89,20 @@ select_key()
 	else
 		select_regexp="$key"
 	fi
-	select_regexp=$(grep '' $db_file | cut -d $delim -f 1 \
+	key=$(grep '' "$db_file" | cut -d $delim -f 1 \
 		| grep "$select_regexp")
+	if [ "k${key}" == "k" ]; then
+		exit 0
+	fi
+	escape_chars_regexp
+	select_regexp=$safe_key
 	if [ $v_key == 1 ]; then
-		for var in $select_regexp; do
-			grep "$var$delim" $db_file
+		for var in "${select_regexp}"; do
+			grep "$var$delim" "$db_file"
 		done
 	else
-		for var in $select_regexp; do
-			grep "$var$delim" $db_file | cut -d $delim -f 2-
+		for var in "${select_regexp}"; do
+			grep "$var$delim" "$db_file" | cut -d $delim -f 2-
 		done
 	fi
 }
@@ -107,7 +118,7 @@ del_key()
 	elif [ $n_args -ge 1 ]; then
 		key=$((i+1))
 		key=${!key}
-		if [ ${key:0:1} == '$' ]; then
+		if [ "k${!key}" != "k" ] && [ ${key:0:1} == '$' ]; then
 			select_key_var
 			key=$key_val
 		fi
@@ -115,18 +126,18 @@ del_key()
 
 		## if no value specified, delete only key's content, not whole  entry
 		if [ $n_args -eq 1 ]; then
-			sed -i "s/\(^$safe_key$delim\).*$/\1/g" $db_file
+			sed -i "s/\(^$safe_key$delim\).*$/\1/g" "$db_file"
 
 		## if a value is specified, delete whole entry if provided value
 		## matches database's one, else do nothing.
 		else
 			value=$((i+2))
 			value=${!value}
-			if [ ${value:0:1} == '$' ]; then
+			if [ "k$value" != "k" ] && [ ${value:0:1} == '$' ]; then
 				select_val_var
 			fi
 			escape_chars_repl
-			sed -i "/^$safe_value$/d" $db_file
+			sed -i "/^$safe_value$/d" "$db_file"
 		fi
 	fi	
 }
@@ -144,36 +155,36 @@ put_key()
 	value=$((i+2))
 	key=${!key}
 	value=${!value}
-	if [ ${key:0:1} == '$' ]; then
+	if [ "k$key" != "k"  ] && [ ${key:0:1} == '$' ]; then
 		select_key_var
 		key=$key_val
 	fi
-	if [ ${value:0:1} == '$' ]; then
+	if [ "k$value" != "k" ] && [ ${value:0:1} == '$' ]; then
 		select_val_var
 	fi
 	escape_chars_regexp
 
 	## if file is empty or does not exists then append new entry.
-	if [ ! -f $db_file ]; then
-		echo "$key$delim$value" >> $db_file
+	if [ ! -f "$db_file" ]; then
+		echo "$key$delim$value" >> "$db_file"
 		return 0
 	fi
 
 	## if key is found in db replace entry else append it.
-	line=$(grep "^$safe_key$delim" $db_file)
+	line=$(grep "^$safe_key$delim" "$db_file")
 	if [ 0 -eq $? ]; then
 		escape_chars_repl
-		sed -i "s/^$safe_key$delim.*$/$safe_value/g" $db_file
+		sed -i "s/^$safe_key$delim.*$/$safe_value/g" "$db_file"
 	else
-		echo "$key$delim$value" >> $db_file
+		echo "$key$delim$value" >> "$db_file"
 	fi
 	return 2
 }
 
 flush_db()
 {
-	if [ -f $db_file ]; then
-		echo -n '' > $db_file
+	if [ -f "$db_file" ]; then
+		echo -n '' > "$db_file"
 	else
 		echo $EFNOENT $db_file
 		exit 1
@@ -185,24 +196,24 @@ get_cmdline()
 	i=$((OPTIND))
 	case ${!i} in
 		"put")
-			put_key $ARGV
+			put_key "$@"
 			;;
 		"flush")
 			flush_db
 			;;
 		"select")
-			if [ ! -f $db_file ]; then
+			if [ ! -f "$db_file" ]; then
 				echo $EFNOENT $db_file
 				exit 1
 			fi
-			select_key $ARGV
+			select_key "$@"
 			;;
 		"del")
-			if [ ! -f $db_file ]; then
+			if [ ! -f "$db_file" ]; then
 				echo $EFNOENT $db_file
 				exit 1
 			fi
-			del_key $ARGV
+			del_key "$@"
 			;;
 		*)
 			echo $ESYNTAX $usage
@@ -235,7 +246,7 @@ get_options()
 }
 
 ARGC=$#
-ARGV=$@
+ARGV="$@"
 get_options "$@"
-get_cmdline $ARGV $OPTIND
+get_cmdline "$@" $OPTIND
 exit 0
