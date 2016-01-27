1e828d00e0dfc993469e43df3a930bf5d6927aa5
k6 <chloe.cano@epitech.eu>
2016-01-27 19:02:37 +0100

    calloc

---
+++ calloc.c
@@ -0,0 +1,12 @@
+#include "malloc.h"
+
+void				*calloc(size_t nmemb, size_t size)
+{
+	unsigned char	*new;
+
+	if (!nmemb || !size)
+		return ((void *)0);
+	new = malloc(nmemb * size);
+	memset(new, 0, size * nmemb);
+	return (new);
+}
